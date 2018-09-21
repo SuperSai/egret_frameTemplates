eff3ad251d40f2ffcdab765ea7116aef0987de28
青楼卧底 <443291877@qq.com>
2018-09-21 16:17:24 +0800

    更新框架
    更新框架

---
+++ src/game/core/manager/GameEnterManager.ts
@@ -0,0 +1,7 @@
+class GameEnterManager extends BaseClass{
+
+	/** 初始化 */
+	public init(): void {
+		App.BoneManager.start();
+	}
+}
---
+++ src/game/core/mvc/model/SkinName.ts
@@ -0,0 +1,11 @@
+class SkinName {
+	public static LoadingUISkin: string = "resource/skins/loading/LoadingUISkin.exml";
+
+	public static LoginSkin: string = "resource/skins/login/LoginSkin.exml";
+
+	public static CurrencyComponentSkin: string = "resource/skins/common/component/CurrencyComponentSkin.exml";
+
+	public static HallViewSkin: string = "resource/skins/hall/HallViewSkin.exml";
+
+
+}
